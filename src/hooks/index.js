50bbf692817ac590afd040b42cff58ae46872b11
Crunchy <crunchy@nugget.local>
2026-04-28 08:07:31 +0800

    feat(srs): add useSRS to hooks barrel

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { usePersistedState } from "./usePersistedState.js";
 export { useQuizKeyboard } from "./useQuizKeyboard.js";
 export { useStreak } from "./useStreak.js";
+export { useSRS } from "./useSRS.js";
